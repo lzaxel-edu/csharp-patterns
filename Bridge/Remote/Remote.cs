5e1ceeb9446f686c7fb43716596e207214f339bc
MivasCode <transnistrianguy@gmail.com>
2024-12-03 18:00:23 +0200

    fix: make TV private field and lowercase

--- Bridge/Remote/Remote.cs
+++ Bridge/Remote/Remote.cs
@@ -4,33 +4,33 @@ namespace Bridge.Remote;
 
 public abstract class Remote
 {
-    private ITV _TV { get; }
+    private ITV _tv;
     public uint CurrentChannel
     {
-        get => _TV.CurrentChannel;
+        get => _tv.CurrentChannel;
     }
 
     public Remote(ITV tv)
     {
         if (tv == null)
         {
-            throw new Exception("TV is null");
+            throw new ArgumentException("TV is null");
         }
-        _TV = tv;
+        _tv = tv;
     }
 
     public virtual void Enable()
     {
-        _TV.Toggle(true);
+        _tv.Toggle(true);
     }
 
     public virtual void Disable()
     {
-        _TV.Toggle(false);
+        _tv.Toggle(false);
     }
 
     public virtual void SetChannel(uint channel)
     {
-        _TV.SetChannel(channel);
+        _tv.SetChannel(channel);
     }
 }
